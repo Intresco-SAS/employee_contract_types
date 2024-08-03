2eace315f6f965120c3f0879fee28f3e5b4ca52b
Nelson V <nelson.avelez@hotmail.com>
2024-08-03 18:49:09 -0500

    corrección de modulo

--- employee_contract_types/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import hr_contract_type
-from . import hr_contract
-
--- employee_contract_types/models/hr_contract.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class HrContract(models.Model):
-    _inherit = "hr.contract"
-
-    contract_type_id = fields.Many2one("hr.contract.type", string="Contract Type")
-
